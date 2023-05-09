287388eb6c4c6fff742e350f4515cbca14e51b41
tanvi1017 <tripathi.tanvi1996@gmail.com>
2023-05-09 08:42:17 +0530

    Initial commit

---
+++ app/src/main/java/com/tanvi/bricodepot/ItemLocationActivity.kt
@@ -0,0 +1,11 @@
+package com.tanvi.bricodepot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_location)
+    }
+}
